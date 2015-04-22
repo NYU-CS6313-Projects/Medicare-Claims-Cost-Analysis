1d97e7badd9cdb9dbb7900a8d51fb20f92fd0e15
Kenneth Luna <kl1771@nyu.edu>
2015-04-22 13:29:51 -0400

    updated merge script

--- mergescript.py
+++ mergescript.py
@@ -1,35 +1,39 @@
-from diagnosis import getFiles
-import csv
-
+from diagnosis import getFiles 
+import csv 
+from datetime import datetime
+startTime = datetime.now()
 
 li = ['DE1_0_2008_to_2010_Outpatient_Claims_Sample_10.csv', 'DE1_0_2008_to_2010_Outpatient_Claims_Sample_11.csv']
 
-def loadBeneficiary(file):
+def loadBeneficiary(file): 
 	with open(file, "rb") as csvfile:
 		beneficiaries = csv.reader(csvfile)
-		headers = beneficiaries.next()
-
+		headers = beneficiaries.next() 
+		
 		patIndex = headers.index('DESYNPUF_ID')
-		chronicIndices = {}
-
-		for patient in beneficiaries:
+		chronicIndices = {} 
+				
+		for patient in beneficiaries: 
 			chronicIndices[patient[patIndex]] = []
 			[chronicIndices[patient[patIndex]].append(patient[i]) for i in range(1, 12)]
+		
+		return chronicIndices 		
 
-		return chronicIndices
+	
 
 def parsePatientFiles(li, dictionary):
-	summary = {}
+	summary = {} 
 	total_uniques = set([])
+	all_chronic = ["alzheimers", "heartFailure", "kidney", "cancer", "COPD", "depression", "diabetes", "ischemicHeart", "osteo", "arthritis", "stroke"] 	
 
 	for file in li:
-		f = open(file)
+		f = open(file) 
 		reader = csv.reader(f)
 		headers = reader.next()
 
 		patIndex = headers.index('DESYNPUF_ID')
-		claimIndex = headers.index('CLM_PMT_AMT')
-
+		claimIndex = headers.index('CLM_PMT_AMT') 
+		
 		diag1_index = headers.index('ICD9_DGNS_CD_1')
         	diag2_index = headers.index('ICD9_DGNS_CD_2')
         	diag3_index = headers.index('ICD9_DGNS_CD_3')
@@ -39,25 +43,64 @@ def parsePatientFiles(li, dictionary):
         	diag7_index = headers.index('ICD9_DGNS_CD_7')
         	diag8_index = headers.index('ICD9_DGNS_CD_8')
         	diag9_index = headers.index('ICD9_DGNS_CD_9')
-        	diag10_index = headers.index('ICD9_DGNS_CD_10')
+        	diag10_index = headers.index('ICD9_DGNS_CD_10')		
 
 		all_diag = [diag1_index, diag2_index, diag3_index, diag4_index, diag5_index, diag6_index, diag7_index, diag8_index, diag9_index, diag10_index]
-
-		for visit in reader:
-			index = 0
+	
+		for visit in reader: 
+			index = 0 
 			while index < 10:
 				if visit[all_diag[index]] == '':
-					index += 1
+					index += 1 
 				else:
-					if visit[all_diag[index]] not in summary:
-						summary[visit[all_diag[index]]] = {}
-						index += 1
-					else:
-						index += 1
+					diag = visit[all_diag[index]]
+					if diag not in summary:
+						summary[diag] = {}
+						summary[diag]["alzheimers"] = 0 
+						summary[diag]["heartFailure"] = 0										
+						summary[diag]["kidney"] = 0 
+						summary[diag]["cancer"] = 0 
+						summary[diag]["COPD"] = 0 
+						summary[diag]["depression"] = 0 
+						summary[diag]["diabetes"] = 0 
+						summary[diag]["ischemicHeart"] = 0 
+						summary[diag]["osteo"] = 0 	
+						summary[diag]["arthritis"] = 0 
+						summary[diag]["stroke"] = 0 	
 
-	print len(summary.keys())
+						for i, illness in enumerate(dictionary[visit[patIndex]]):
+							if illness == '1.0': 
+								summary[diag][all_chronic[i]] += 1 
+							elif illness == '2.0':
+								pass 
+							else:
+								pass  
+								
+					else:
+						for i, illness in enumerate(dictionary[visit[patIndex]]):
+                                                        if illness == '1.0':
+                                                                summary[diag][all_chronic[i]] += 1
+                                                        elif illness == '2.0':
+                                                                pass
+                                                        else:
+                                                                pass
+					
+					#print diag, summary[diag]	
+					index += 1 	
+		
+	print datetime.now() - startTime									
+	
+					#print dictionary[visit[patIndex]]
+					#index += 1   
 
 
 if __name__=='__main__':
-	chronic = loadBeneficiary('patient_summary.csv')
-	parsePatientFiles(li, chronic)
+	chronic = loadBeneficiary('patient_summary.csv') 
+	x, y = getFiles()
+	x.extend(y) 
+	parsePatientFiles(x, chronic)	
+
+
+
+
+
