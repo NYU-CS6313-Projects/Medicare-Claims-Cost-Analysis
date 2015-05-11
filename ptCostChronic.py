ae0437a9cf2e07e12054bb838a2c03894722336e
Kenneth Luna <kl1771@nyu.edu>
2015-05-11 06:58:19 -0400

    andrews file

---
+++ ptCostChronic.py
@@ -0,0 +1,73 @@
+import csv
+import json
+
+def readBenSum(filename, chronics):
+  
+  with open(filename, "rb") as csvfile:
+    beneficiaries = csv.reader(csvfile)
+    headers = beneficiaries.next()
+    
+    patIndex = headers.index('DESYNPUF_ID')
+
+    inpatientCostIndex = headers.index('MEDREIMB_IP')
+    outpatientCostIndex = headers.index('MEDREIMB_OP')
+    carrierCostIndex = headers.index('MEDREIMB_CAR')
+
+    chronicIndices = {}
+    chronicIndices["alzheimers"]    = headers.index('SP_ALZHDMTA')
+    chronicIndices["heartFailure"]  = headers.index('SP_CHF')
+    chronicIndices["kidney"]        = headers.index('SP_CHRNKIDN')
+    chronicIndices["cancer"]        = headers.index('SP_CNCR')
+    chronicIndices["COPD"]          = headers.index('SP_COPD')
+    chronicIndices["depression"]    = headers.index('SP_DEPRESSN')
+    chronicIndices["diabetes"]      = headers.index('SP_DIABETES')
+    chronicIndices["ischemicHeart"] = headers.index('SP_ISCHMCHT')
+    chronicIndices["osteo"]         = headers.index('SP_OSTEOPRS')
+    chronicIndices["arthritis"]     = headers.index('SP_RA_OA')
+    chronicIndices["stroke"]        = headers.index('SP_STRKETIA')
+
+    patientStuff = {}
+
+    for patient in beneficiaries:
+      patID = patient[patIndex]
+      inpatientCost = float(patient[inpatientCostIndex])
+      outpatientCost = float(patient[outpatientCostIndex])
+      carrierCost = float(patient[carrierCostIndex])
+      totalCost = inpatientCost + outpatientCost + carrierCost
+      # toggle here to eliminate 0 cost
+      if totalCost > minCost:
+        if patID not in patientStuff:
+          patientStuff[patID] = {"cost": totalCost}
+          patientChronic = []
+          for i in chronics:
+            if patient[chronicIndices[i]] == '1':
+              patientChronic.append(i)
+          patientStuff[patID]["chronics"] = patientChronic
+        else:
+          patientStuff[patID]["cost"] += totalCost
+          for i in chronics:
+            if patient[chronicIndices[i]] == '1':
+              if i not in patientStuff[patID]["chronics"]:
+                patientStuff[patID]["chronics"].append(i)
+
+  return (patientStuff)
+
+
+def jsonDump(dict, writeFile):
+  with open(writeFile, 'wb') as fp:
+    json.dump(dict, fp)
+  print "done writing", writeFile
+
+
+if __name__=='__main__':
+  minCost = 0
+  chronicDis = ["alzheimers", "heartFailure", "kidney", "cancer", "COPD", "depression", "diabetes", "ischemicHeart", "osteo", "arthritis", "stroke"]
+
+  benSumFile = "DE1_0_2008_Beneficiary_Summary_File_Sample_1.csv"
+  jsonWriteFile = "ptCostChronic.json"
+
+  ptStuffList = readBenSum(benSumFile, chronicDis)
+  # for i in ptStuffList:
+  #   print i, ptStuffList[i]["cost"], ptStuffList[i]["chronics"]
+
+  jsonDump(ptStuffList, jsonWriteFile)
