ae5611251b1c5624763a2e4a69f7a3ee227015fa
danoclarke <danielrichardclarke@gmail.com>
2015-05-16 20:42:36 -0400

    Latest Front End

---
+++ data/slider/ptCostPercentiles.py
@@ -0,0 +1,67 @@
+import csv
+import json
+from beneficiaryORG import getFiles 
+
+def readBenSum(list):
+
+  patientCosts = [] 
+
+  for item in list:
+    loc = "../" + item 
+    with open(loc, "rb") as csvfile:
+      beneficiaries = csv.reader(csvfile)
+      headers = beneficiaries.next()
+    
+      patIndex = headers.index('DESYNPUF_ID')
+      inpatientCostIndex = headers.index('MEDREIMB_IP')
+      outpatientCostIndex = headers.index('MEDREIMB_OP')
+      carrierCostIndex = headers.index('MEDREIMB_CAR')
+
+      for patient in beneficiaries:
+        patID = patient[patIndex]
+        inpatientCost = float(patient[inpatientCostIndex])
+        outpatientCost = float(patient[outpatientCostIndex])
+        carrierCost = float(patient[carrierCostIndex])
+        totalCost = inpatientCost + outpatientCost + carrierCost
+        patientCosts.append((patID, totalCost))
+
+    return (patientCosts)  
+
+def getPercentiles(ptCosts):
+
+  ptCosts.sort(key=lambda x: x[1])
+  numPts = len(ptCosts)
+
+  pctileDict = {}
+  for i in range(100):
+    pctileDict[i] = {}
+    pctileDict[i]["patients"] = []
+    reverseIndex = numPts*i/100
+    pctileDict[i]["costFloor"] = ptCosts[reverseIndex][1]
+
+  for i in range(len(ptCosts)):
+    index = int(i*100/numPts)
+    pctileDict[index]["patients"].append(ptCosts[i][0])
+
+  # for i in pctileDict:
+  #   print i, len(pctileDict[i]["patients"]), pctileDict[i]["costFloor"], "\n"
+
+  return(pctileDict)
+
+
+def jsonDump(dict, writeFile):
+  with open(writeFile, 'wb') as fp:
+    json.dump(dict, fp)
+  print "done writing", writeFile
+
+
+if __name__=='__main__':
+ 
+  benSumFile = getFiles()  
+  jsonWriteFile = "ptsByPercentile.json"
+
+  ptCostList = readBenSum(benSumFile)
+  ptsByPercentile = getPercentiles(ptCostList)
+  jsonDump(ptsByPercentile, jsonWriteFile)
+
+	
