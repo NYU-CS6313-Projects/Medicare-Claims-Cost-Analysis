8810656a4f8aa30f1b1074ad54f88a5bdeb53ddf
krl22785 <krluna@gmail.com>
2015-06-20 19:22:57 -0400

    updates

--- claimVectorize.py
+++ claimVectorize.py
@@ -2,21 +2,38 @@ import os
 import csv
 import json 
 import numpy  
-from beneficiaryAttributesDB import getCMSFiles, beneficiaryTable
+from beneficiaryAttributesDB import getCMSFiles, buildBeneficiaryTable
 
 
-def extractClaims(list):
-
+def extractClaims(list, db):
+	cnt = 0 
 	for file in list: 
+		loc = directory + "/" + file 
+		f = open(loc) 
+		reader = csv.reader(f) 
+
+		headers = reader.next() 
 		
-	
+		desynpuf_id_index = headers.index('DESYNPUF_ID')	
+		clm_from_dt_index = headers.index("CLM_FROM_DT")
+		clm_thru_dt_index = headers.index("CLM_THRU_DT") 
+		clm_pmt_amt_index = headers.index("CLM_PMT_AMT") 
+		clm_utlztn_cnt_index = headers.index('CLM_UTLZTN_DAY_CNT')
+		
+		for visit in reader:
+			patientID = visit[desynpuf_id_index] 
+			if patientID in db:
+				print visit[clm_utlztn_cnt_index], visit[clm_pmt_amt_index]  	 
+				cnt += 1 
+			else: 
+				print 'not in db' 
 
-
+	print "HERE IS HOW MNAY OBS %s" % cnt 
 if __name__=='__main__':
 	directory = os.environ['health_path']
-	files = getFiles(directory, 'inpatient') 
+	files = getCMSFiles(directory, 'inpatient') 
 	
 	f = open("beneficiaryDB.json") 
 	beneficiaryDB = json.load(f) 
 
-	newMethod(files0 
+	extractClaims(files, beneficiaryDB) 
