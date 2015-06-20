2f1a777913f3256900e8c2c7935870dc91b90ea8
krl22785 <krluna@gmail.com>
2015-06-20 15:42:29 -0400

    add updates

--- beneficiaryAttributesDB.py
+++ beneficiaryAttributesDB.py
@@ -1,18 +1,49 @@
 import csv 
 import os 
+import json 
+
+"""
+   use the following keywords:  
+   beneficiary - Beneficiary Summary Files 
+   carrier - Carrier Claim Files 
+   inpatient - Inpatient Claim Files 
+   outpatient - Outpatient Claim Files 
+   prescription - Prescription Drug Events 
+""" 
+
+keywords = {
+	'beneficiary': 'Beneficiary_Summary_File',
+	'carrier': 'Carrier_Claims', 
+	'inpatient': 'Inpatient_Claims_Sample', 
+ 	'outpatient': 'Outpatient_Claims_Sample',
+	'prescription': 'Prescription_Drug_Events'
+	}
+
+def getCMSFiles(directory, string):
+	
+	keywords = {
+        'beneficiary': 'Beneficiary_Summary_File',
+        'carrier': 'Carrier_Claims',
+        'inpatient': 'Inpatient_Claims_Sample',
+        'outpatient': 'Outpatient_Claims_Sample',
+        'prescription': 'Prescription_Drug_Events'
+        }
+	
+	try: 
+		fileList = [] 	
+		keywordFilter = keywords[string]  
+		
+		for file in os.listdir(directory): 
+			if keywordFilter in file:
+				fileList.append(file) 
+			else:
+				pass 
+		return fileList 
+	except: 
+		return "Use this dictionary %s" % keywords
 
 
-def getFiles(directory):
-	fileList = [] 	
-	for file in os.listdir(directory): 
-		if 'Beneficiary_Summary_File' in file:
-			fileList.append(file) 
-		else:
-
-			pass 
-	return fileList 
-
-def beneficiaryTable(list, directory): 
+def buildBeneficiaryTable(list, directory, dumpToFile_Boolean): 
 	beneficiaryDB = {} 
 	
 	for file in list:
@@ -33,28 +64,35 @@ def beneficiaryTable(list, directory):
 		diabetes_index = headers.index('SP_DIABETES') 
 		heart_index = headers.index('SP_CHF') 
 		
-		for patient in reader: 
-			desynpuf_id = patient[desynpuf_id_index] 
-			
-			if desynpuf_id not in beneficiaryDB:
-				beneficiaryDB[desynpuf_id] = {} 
-				beneficiaryDB[desynpuf_id]['birth_date'] = patient[birth_date_index]
-				beneficiaryDB[desynpuf_id]['sex'] = patient[sex_index]
-				beneficiaryDB[desynpuf_id]['race'] = patient[race_index] 
-				beneficiaryDB[desynpuf_id]['state'] = patient[state_index] 
-				beneficiaryDB[desynpuf_id]['county'] = patient[county_index] 
-				beneficiaryDB[desynpuf_id][year] = {}
-				beneficiaryDB[desynpuf_id][year]['diabetes'] = patient[diabetes_index] 
-				beneficiaryDB[desynpuf_id][year]['heart'] = patient[heart_index]  
-			else:
-				beneficiaryDB[desynpuf_id][year] = {}
-                                beneficiaryDB[desynpuf_id][year]['diabetes'] = patient[diabetes_index]
-                                beneficiaryDB[desynpuf_id][year]['heart'] = patient[heart_index]
-
-	return beneficiaryDB
+		for patient in reader:
+			if patient[diabetes_index] == '1' or patient[heart_index] == '1':
+				desynpuf_id = patient[desynpuf_id_index] 
 				
-#if __name__=='__main__':
-#	directory = os.environ['health_path']
-#	files = getFiles(directory) 
-#	beneficiaryTable(files, directory) 
+				if desynpuf_id not in beneficiaryDB:
+					beneficiaryDB[desynpuf_id] = {} 
+					beneficiaryDB[desynpuf_id]['birth_date'] = patient[birth_date_index]
+					beneficiaryDB[desynpuf_id]['sex'] = patient[sex_index]
+					beneficiaryDB[desynpuf_id]['race'] = patient[race_index] 
+					beneficiaryDB[desynpuf_id]['state'] = patient[state_index] 
+					beneficiaryDB[desynpuf_id]['county'] = patient[county_index] 
+					beneficiaryDB[desynpuf_id][year] = {}
+					beneficiaryDB[desynpuf_id][year]['diabetes'] = patient[diabetes_index] 
+					beneficiaryDB[desynpuf_id][year]['heart'] = patient[heart_index]  
+				else:
+					beneficiaryDB[desynpuf_id][year] = {}
+					beneficiaryDB[desynpuf_id][year]['diabetes'] = patient[diabetes_index]
+					beneficiaryDB[desynpuf_id][year]['heart'] = patient[heart_index]
+			else:
+				pass 
+
+	if dumpToFile_Boolean:
+		with open("beneficiaryDB.json", 'wb') as fp: 
+			json.dump(beneficiaryDB, fp) 
+	else: 
+		return beneficiaryDB
+				
+if __name__=='__main__':
+	directory = os.environ['health_path']
+	files = getCMSFiles(directory, 'beneficiary')
+	buildBeneficiaryTable(files, directory, True) 
 
--- claimVectorize.py
+++ claimVectorize.py
@@ -1,60 +1,22 @@
-import csv 
 import os 
+import csv
+import json 
+import numpy  
+from beneficiaryAttributesDB import getCMSFiles, beneficiaryTable
 
 
-def getFiles(directory):
-	fileList = [] 	
-	for file in os.listdir(directory): 
-		if 'Beneficiary_Summary_File' in file:
-			fileList.append(file) 
-		else:
+def extractClaims(list):
 
-			pass 
-	return fileList 
-
-def beneficiaryTable(list, directory): 
-	beneficiaryDB = {} 
+	for file in list: 
+		
 	
-	for file in list:
-		year = file[6:10]
-		loc = directory + "/" + file 
 
-		f = open(loc) 
-		reader = csv.reader(f) 
 
-		headers = reader.next() 
-		desynpuf_id_index = headers.index('DESYNPUF_ID') 
-		birth_date_index = headers.index('BENE_BIRTH_DT')
-		sex_index = headers.index('BENE_SEX_IDENT_CD') 
-		race_index = headers.index('BENE_RACE_CD') 
-		state_index = headers.index('SP_STATE_CODE') 
-		county_index = headers.index('BENE_COUNTY_CD') 
-		
-		diabetes_index = headers.index('SP_DIABETES') 
-		heart_index = headers.index('SP_CHF') 
-		
-		for patient in reader: 
-			desynpuf_id = patient[desynpuf_id_index] 
-			
-			if desynpuf_id not in beneficiaryDB:
-				beneficiaryDB[desynpuf_id] = {} 
-				beneficiaryDB[desynpuf_id]['birth_date'] = patient[birth_date_index]
-				beneficiaryDB[desynpuf_id]['sex'] = patient[sex_index]
-				beneficiaryDB[desynpuf_id]['race'] = patient[race_index] 
-				beneficiaryDB[desynpuf_id]['state'] = patient[state_index] 
-				beneficiaryDB[desynpuf_id]['county'] = patient[county_index] 
-				beneficiaryDB[desynpuf_id][year] = {}
-				beneficiaryDB[desynpuf_id][year]['diabetes'] = patient[diabetes_index] 
-				beneficiaryDB[desynpuf_id][year]['heart'] = patient[heart_index]  
-			else:
-				beneficiaryDB[desynpuf_id][year] = {}
-                                beneficiaryDB[desynpuf_id][year]['diabetes'] = patient[diabetes_index]
-                                beneficiaryDB[desynpuf_id][year]['heart'] = patient[heart_index]
+if __name__=='__main__':
+	directory = os.environ['health_path']
+	files = getFiles(directory, 'inpatient') 
+	
+	f = open("beneficiaryDB.json") 
+	beneficiaryDB = json.load(f) 
 
-	return beneficiaryDB
-				
-#if __name__=='__main__':
-#	directory = os.environ['health_path']
-#	files = getFiles(directory) 
-#	beneficiaryTable(files, directory) 
-
+	newMethod(files0 
