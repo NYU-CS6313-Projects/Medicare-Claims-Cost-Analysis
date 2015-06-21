15596d3af2afe35f75538998670e1d917f1ea13c
krl22785 <krluna@gmail.com>
2015-06-21 09:19:15 -0400

    updates to claimVectorize

--- claimVectorize.py
+++ claimVectorize.py
@@ -6,7 +6,9 @@ from beneficiaryAttributesDB import getCMSFiles, buildBeneficiaryTable
 
 
 def extractClaims(list, db):
-	cnt = 0 
+	data = [] 
+	counterClaims = {} 
+
 	for file in list: 
 		loc = directory + "/" + file 
 		f = open(loc) 
@@ -20,20 +22,58 @@ def extractClaims(list, db):
 		clm_pmt_amt_index = headers.index("CLM_PMT_AMT") 
 		clm_utlztn_cnt_index = headers.index('CLM_UTLZTN_DAY_CNT')
 		
+		"""we are only summing the number diagnosis received by the patient 
+		   per visits""" 
+		
+		diag1_index = headers.index('ICD9_DGNS_CD_1')
+        	diag2_index = headers.index('ICD9_DGNS_CD_2')
+        	diag3_index = headers.index('ICD9_DGNS_CD_3')
+        	diag4_index = headers.index('ICD9_DGNS_CD_4')
+        	diag5_index = headers.index('ICD9_DGNS_CD_5')
+        	diag6_index = headers.index('ICD9_DGNS_CD_6')
+        	diag7_index = headers.index('ICD9_DGNS_CD_7')
+        	diag8_index = headers.index('ICD9_DGNS_CD_8')
+        	diag9_index = headers.index('ICD9_DGNS_CD_9')
+        	diag10_index = headers.index('ICD9_DGNS_CD_10')
+
+		diagList = [diag1_index, diag2_index, diag3_index, diag4_index, diag5_index, diag6_index, diag7_index, diag8_index, diag9_index, diag10_index]
+
 		for visit in reader:
 			patientID = visit[desynpuf_id_index] 
+			claimAmount = visit[clm_pmt_amt_index]
+			claimStartDate = visit[clm_from_dt_index] 
+			lengthOfStay = visit[clm_utlztn_cnt_index] 
+			
+			index = 0 
+			diagnosisCount = 0  		
+	
+			while index < 10:
+				if visit[diagList[index]] != '': 
+					diagnosisCount += 1 
+					index += 1
+				else:
+					index = 10 			
+	
 			if patientID in db:
-				print visit[clm_utlztn_cnt_index], visit[clm_pmt_amt_index]  	 
-				cnt += 1 
-			else: 
-				print 'not in db' 
+				rowData = [patientID, claimAmount, claimStartDate, lengthOfStay, diagnosisCount]
+				data.append(rowData) 
+				
+				if patientID not in counterClaims: 	
+					counterClaims[patientID] = {} 
+					counterClaims[patientID][claimStartDate] = lengthOfStay 		
+				else:
+					counterClaims[patientID][claimStartDate] = lengthOfStay
 
-	print "HERE IS HOW MNAY OBS %s" % cnt 
+			else:
+				pass 
+
+	print counterClaims
+		
 if __name__=='__main__':
 	directory = os.environ['health_path']
-	files = getCMSFiles(directory, 'inpatient') 
+	files = getCMSFiles(directory, 'inpatient')[0:5]  
 	
 	f = open("beneficiaryDB.json") 
 	beneficiaryDB = json.load(f) 
-
+	dict = {} 
 	extractClaims(files, beneficiaryDB) 
